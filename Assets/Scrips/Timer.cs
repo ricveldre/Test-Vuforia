e8532e6d08face2544a6193eaed1bbb662a07a64
ricveldre <ricardo.velazquez@uteca.edu.mx>
2025-05-20 20:29:07 -0600

    Timer

---
+++ Assets/Scrips/Timer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _onTimerComplete;
+    [SerializeField]
+    private UnityEvent<string> _OnSecondPassed;
+    private Coroutine _TimerCoroutine;
+    public void StartTimer(float duration)
+    {
+        StartCoroutine(RunTimer(duration));
+    }
+    private IEnumerator RunTimer(float duration)
+    {
+        _OnSecondPassed?.Invoke(""+(int)duration);
+        yield return new WaitForSeconds(duration);
+        if (duration == 0)
+        {
+            _onTimerComplete?.Invoke();
+            _TimerCoroutine = null;
+        }
+        else
+        {
+            _TimerCoroutine = StartCoroutine(RunTimer(duration - 1));
+        }
+    }
+    private void StopTimer()
+    {
+        if (_TimerCoroutine != null)
+        {
+            StopCoroutine(_TimerCoroutine);
+            _TimerCoroutine = null;
+        }
+    }
+}
+    
